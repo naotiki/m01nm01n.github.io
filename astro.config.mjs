1c19e37378b292a0af85e7d8df6d84d85b464edc
naotiki <contact@naotiki.me>
2025-04-16 21:53:14 +0900

    style: fmt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://m01nm01n.github.io",
+  integrations: [mdx(), sitemap()],
+  i18n: {
+    locales: ["ja", "en"],
+    defaultLocale: "ja",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
