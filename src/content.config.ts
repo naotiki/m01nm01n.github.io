02f77eeb411a16ebd0464bf2012ad5abec95207e
naotiki <contact@naotiki.me>
2025-04-30 00:20:36 +0900

    fix: enable `sharp` image optimizer

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,46 +16,47 @@ const blog = defineCollection({
 
 const contests = defineCollection({
   loader: glob({ base: "./src/content/contests", pattern: "**/*.{md,mdx}" }),
-  schema: z.object({
-    // 大会タイトル
-    title: z.string(),
-    // 開催された日時 (JST)
-    startDate: z.coerce.date(),
-    endDate: z.coerce.date(),
-    // 順位
-    result: z.string(),
-    // 順位を目立たせるかどうか
-    highlighted: z.boolean().optional(),
-    // タグ
-    tags: z.enum(["writeup"]).array(),
-    members: z.array(reference("member")),
-    // 投稿日時 (JST)
-    pubDate: z.coerce.date(),
-    // heroImage
-    heroImage: z.string().optional(),
-    // 大会公式などのURL
-    externalUrl: z.string().url().optional(),
-    ctfTimeUrl: z.string().url().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      // 大会タイトル
+      title: z.string(),
+      // 開催された日時 (JST)
+      startDate: z.coerce.date(),
+      endDate: z.coerce.date(),
+      // 順位
+      result: z.string(),
+      // 順位を目立たせるかどうか
+      highlighted: z.boolean().optional(),
+      // タグ
+      tags: z.enum(["writeup"]).array(),
+      members: z.array(reference("member")),
+      // 投稿日時 (JST)
+      pubDate: z.coerce.date(),
+      // heroImage
+      heroImage: image().optional(),
+      // 大会公式などのURL
+      externalUrl: z.string().url().optional(),
+      ctfTimeUrl: z.string().url().optional(),
+    }),
 });
 
 const member = defineCollection({
   loader: glob({ base: "./src/content/members", pattern: "**/*.{md,mdx}" }),
-  schema: z.object({
-    // 名前 (表示用)
-    name: z.string(),
-    // 本名 (opt)
-    realName: z.string().optional(),
-    // 自己紹介 (opt)
-    description: z.string().optional(),
-    // スキル (opt)
-    skills: z.string().array().optional(),
-    // アイコン public/images/members/に置く
-    image: z.string().startsWith("/").optional(),
-    x_twitter: z.string().url().optional(),
-    github: z.string().url().optional(),
-    website: z.string().url().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      // 名前 (表示用)
+      name: z.string(),
+      // 本名 (opt)
+      realName: z.string().optional(),
+      // 自己紹介 (opt)
+      description: z.string().optional(),
+      // スキル (opt)
+      skills: z.string().array().optional(),
+      image: image(),
+      x_twitter: z.string().url().optional(),
+      github: z.string().url().optional(),
+      website: z.string().url().optional(),
+    }),
 });
 
 export const collections = { blog, member, contests };
